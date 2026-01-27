606f8dd3333309e190e3cbe061b7454ee19d1e23
cmccabe <conor.mccabe@patmos.tech>
2026-01-27 06:54:20 -0700

    add family sports

--- backend/scraper.py
+++ backend/scraper.py
@@ -35,6 +35,8 @@ COURSES = [
     {"provider": "chronogolf", "course_id": "001a7f2d-2c20-4bd9-8f91-3df9d051f737", "name": "Lone Tree 18 Hole", "booking_slug": "lone-tree-golf-club-hotel"},
     # Littleton (Chronogolf)
     {"provider": "chronogolf", "course_id": "6a1ad175-7c4f-4692-a58f-7879e72ed9e9", "name": "Littleton Golf & Tennis", "booking_slug": "littleton-golf-tennis-club"},
+    # Family Sports (Chronogolf)
+    {"provider": "chronogolf", "course_id": "34b44f75-a475-4ec1-b5d3-e3089b66cf86", "name": "Family Sports 9 Hole", "booking_slug": "family-sports-golf-course"},
 ]
 
 def get_headers():
